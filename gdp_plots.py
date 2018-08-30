3b25bd7bddd00b5f80788f7565e50b430d493618
Jacob Munger <jacobmunger@yahoo.com>
2018-08-30 15:19:50 -0500

    added checks for arguments and flags

--- gdp_plots.py
+++ gdp_plots.py
@@ -9,9 +9,19 @@ import glob
 # the columns and their gdp data becomes the rows
 
 # read data into a pandas dataframe and transpose
+if len(sys.argv) == 1:
+	#no arguments supplied
+	print("Not enough arguments have been provided")
+	print("Usage: python gdp_plotspy <filenames>")
+	print("Options: -a: plot all data in directory")
+	exit()
 
 if sys.argv[1] == '-a':
 	file_list = glob.glob("*gdp*.csv")
+	if len(file_list)==0:
+		print("No files found in current directory.")
+		exit()
+	
 else:
 	file_list = sys.argv[1:]
 
